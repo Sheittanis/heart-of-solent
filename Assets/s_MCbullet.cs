b965f3e9f5ee3d06b26c75ac6eb4f9af6eaf8aec
Sheittanis <shei_ellas@hotmail.com>
2020-04-10 18:33:34 +0100

    meta

---
+++ Assets/s_MCbullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class s_MCbullet : MonoBehaviour {
+
+    public float timer;
+    public bool exists;
+
+    // Use this for initialization
+    void Start()
+    {
+        timer = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            //autodestruct
+            destruction();
+        }
+    }
+
+    public void destruction()
+    {
+        Destroy(gameObject); //destroy this shit
+    }
+
+    public void OnCollisionEnter2D(Collision2D Col)
+    {
+        if (Col.gameObject.tag == "Player" || Col.gameObject.tag == "Boundary") //destroyed if it touches the boundaries
+        {
+            Destroy(gameObject);
+            Col.gameObject.GetComponent<s_heart>().Death(); //rip
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
+
---
+++ Assets/s_heart.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class s_heart : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public bool MControlling;
+
+    public float Rtimer = 3.0f;
+
+    private SpriteRenderer playerSprite;
+    private Rigidbody2D rb;
+
+    public int deathCounter = 0;
+    public int deathRestart = 0;
+    public int explosionCounter = 0;
+    // Use this for initialization
+    void Start()
+    {
+        playerSprite = GetComponent<SpriteRenderer>();
+        MControlling = false;
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!MControlling)//is active
+        {
+
+            //float moveHorizontal = Input.GetAxis("Horizontal");
+            //float moveVertical = Input.GetAxis("Vertical");
+
+            //Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+            //rb.AddForce(movement * speed);
+            //Forward movement
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector2.up * Time.deltaTime * speed);
+                //playerSprite.flipX = true;
+            }//endofW
+            
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector2.down * Time.deltaTime * speed);
+            }//endofS
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector2.left * Time.deltaTime * speed);
+                //playerSprite.flipX = true;
+            }//endofA
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector2.right * Time.deltaTime * speed);
+                //playerSprite.flipX = true;
+            }//endofD
+
+
+            if (Input.GetKeyDown(KeyCode.E)) //&&doesnotexist
+            {
+                //shootbullet
+            }//endofMC
+
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Rtimer -= Time.deltaTime;
+                if (Rtimer < 0)
+                {
+                    //autodestruct
+                    Death();
+                    deathRestart++;
+                }
+
+            }//endofRestart
+            else
+            {
+                Rtimer = 3.0f;
+            }
+        }//endofHeartControl
+    }//endofUpdate
+
+    public void Death()
+    {
+        //pause and screen stuff
+        Destroy(this);
+        deathCounter++;
+    }
+}
